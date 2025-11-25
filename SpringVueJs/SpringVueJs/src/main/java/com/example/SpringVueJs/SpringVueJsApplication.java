ee71b8fd8dfe7eac9f27c339fd6941c5cb25c299
ShivamRai <shivamraid232002@gmail.com>
2025-11-26 01:49:11 +0530

    Revert "Merge pull request #5 from ShivamRai0420/feature/checking_revert"
    This reverts commit 46ebfdf140ad4cde1d2f4e5be1c58b51b1b5174f, reversing
    changes made to c43d12ba74302af5d6634d0ead03b6846a363ff7.

--- SpringVueJs/SpringVueJs/src/main/java/com/example/SpringVueJs/SpringVueJsApplication.java
+++ SpringVueJs/SpringVueJs/src/main/java/com/example/SpringVueJs/SpringVueJsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringVueJsApplication {
 //Commit
-    //just revert this
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVueJsApplication.class, args);
 	}
